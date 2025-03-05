ac74e56bda347c18a1ba0face7f7cd0a1937e515
bmredden <bentleyredden@gmail.com>
2025-03-05 09:41:52 -0700

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import teamData from "/Users/bentleyredden/Desktop/IS 413/Mission09_Redden/frontend/src/teams.json"
+import MMLogo from "/Users/bentleyredden/Desktop/IS 413/Mission09_Redden/frontend/src/assets/mmlogo.png"
+import "./App.css";
+
+function Welcome() {
+  return (
+    <>
+      <img src={MMLogo} alt="March Madness Logo" style={{ width: "700px", height: "auto", marginTop: "10px" }} />
+      <h2>Follow your favorite teams through this year's bracket!</h2>
+      <br/>
+    </>
+  );
+}
+
+function Team({school, name, city, state,}: {school: string; name: string; city: string; state: string;}) {
+  return (
+    <>
+      <h2>
+        {school}
+        <br/>
+        {name}
+      </h2>
+      <h3>Location: {city}, {state}</h3>
+    </>
+  );
+}
+
+function TeamList() {
+  return (
+    <>
+      {teamData.teams.map((singleTeam) => (
+        <>
+          <Team {...singleTeam} />
+          <br/>
+        </>
+      ))}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      < Welcome />
+      <TeamList />
+    </>
+  );
+}
+
+export default App;
